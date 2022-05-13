6c8d20da1c478245fdc87bf4bc9a3d2f448137a7
AliAkeem <78475571+Ali-Akeem@users.noreply.github.com>
2022-05-13 21:07:48 +0200

    conditional rendering

--- pages/api/tasks/[taskId].js
+++ pages/api/tasks/[taskId].js
@@ -1,24 +1,23 @@
 import { tasks } from "../../../data/tasks";
 
-export default function handler(req, res) {
+export default function handler(req, res){
     const { taskId, tasktxt } = req.query
-    
 
     if(req.method === 'GET'){
         const task = tasks.find(
             (task) => task.id === parseInt(taskId)
         )
         res.status(200).json(task)
-    }else if (req.method === 'DELETE'){
-        const deletedTask = tasks.find(
+    }else if(req.method === 'DELETE'){
+        const delTask = tasks.find(
             (task) => task.id === parseInt(taskId)
         )
         const index = tasks.findIndex(
-            (task) => task.id === parseInt(taskId)
+            (task) => task.id ===parseInt(taskId)
         )
-        tasks.splice(index, 1)
-        res.status(200).json(deletedTask)
-    } else if (req.method === 'PUT'){
+        tasks.splice(index,1)
+        res.status(200).json(delTask)
+    }else if (req.method === 'PUT'){
 
         const updTask = tasks.find(
             (task) => task.id == parseInt(taskId)
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function TasksPage() {
 
   const [tasks, setTasks] = useState([])
   const [task, setTask] = useState('')
+  const [updating, setUpdating] = useState(false)
 
   const fetchTasks = async () => {
     const response = await fetch('api/tasks')
@@ -12,6 +13,8 @@ function TasksPage() {
   }
 
   const submitTask = async () => {
+    setTask('')
+    setUpdating(true)
     const response = await fetch('api/tasks', {
       method: 'POST',
       body: JSON.stringify({ task }),
@@ -31,6 +34,7 @@ function TasksPage() {
   }
 
   const updateTask = async taskId => {
+    setUpdating(false)
     const response = await fetch(`/api/tasks/${taskId}`, {
       method: 'PUT',
       body: JSON.stringify({ task }),
@@ -39,26 +43,47 @@ function TasksPage() {
       }
     })
   }
+  
 
   return (
+
     <div className="container">
+
       <h1 className="heading">toDo List</h1>
+
       <input 
         className="writtingpad"
         type='text'
         value={task}
-        onChange={ e => setTask(e.target.value)}
+        onChange={ 
+          e => {
+            setTask(e.target.value)
+          }
+        }
       />
 
-      <button className="submitbtn" onClick={submitTask}>Submit</button>
-      
-      <button className="viewbtn" onClick={fetchTasks}>View tasks</button>
+      { updating ? 
+        <button 
+          className="updatebtn"
+          onClick={() => updateTask(task.id)}
+        >
+          Update
+       </button> :
+       <button 
+          className="submitbtn" 
+          onClick={submitTask}
+        > 
+          Save
+        </button>
+      }
+     
+      {/** <button className="viewbtn" onClick={fetchTasks}>View tasks</button> */}
 
       {
         tasks.map( task => {
           return(
             <div className="taskcontainer" key={task.id}>
-                {task.text}
+                <span>{task.text}</span>
                 <button 
                   className="deletebtn" 
                   onClick={ () => deleteTask(task.id)}  
