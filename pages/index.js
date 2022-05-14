7233d1d050d97b49240a3504d76e32445e4fad89
Imran Artwel <imran.artwell@gmail.com>
2022-05-14 17:25:22 -0500

    [CHORE] made json server to handle requests

--- pages/index.js
+++ pages/index.js
@@ -1,107 +1,105 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [updating, setUpdating] = useState(false);
+  const [updTask, setUpdTask] = useState({});
 
-  const [tasks, setTasks] = useState([])
-  const [task, setTask] = useState('')
-  const [updating, setUpdating] = useState(false)
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   const fetchTasks = async () => {
-    const response = await fetch('api/tasks')
-    const data = await response.json()
-    setTasks(data)
-  }
+    const response = await fetch("api/tasks");
+    const data = await response.json();
+    setTasks(data);
+  };
 
   const submitTask = async () => {
-    setTask('')
-    setUpdating(true)
-    const response = await fetch('api/tasks', {
-      method: 'POST',
+    await fetch("/api/tasks", {
+      method: "POST",
       body: JSON.stringify({ task }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json()
-    fetchTasks()
-  }
+        "Content-Type": "application/json",
+      },
+    });
+    setTask("");
+    fetchTasks();
+  };
 
-  const deleteTask = async taskId => {
-    const response = await fetch(`/api/tasks/${taskId}`, {
-      method: 'DELETE',
-    })
-    fetchTasks()
-  }
+  const deleteTask = async (taskId) => {
+    await fetch(`/api/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+    fetchTasks();
+  };
 
-  const updateTask = async taskId => {
-    setUpdating(false)
-    const response = await fetch(`/api/tasks/${taskId}`, {
-      method: 'PUT',
+  const updateTask = async (taskId) => {
+    await fetch(`/api/tasks/${taskId}`, {
+      method: "PUT",
       body: JSON.stringify({ task }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-  }
-  
+        "Content-Type": "application/json",
+      },
+    });
+    setTask("");
+    fetchTasks();
+    setUpdating(false);
+  };
+
+  const triggerTaskUpdate = (taskId) => {
+    setUpdating(true);
+    const taskToUpdate = tasks.find((task) => task.id === taskId);
+    setTask(taskToUpdate.text);
+    setUpdTask(taskToUpdate);
+  };
 
   return (
-
     <div className="container">
-
       <h1 className="heading">toDo List</h1>
 
-      <input 
+      <input
         className="writtingpad"
-        type='text'
+        type="text"
         value={task}
-        onChange={ 
-          e => {
-            setTask(e.target.value)
-          }
-        }
+        onChange={(e) => {
+          setTask(e.target.value);
+        }}
       />
 
-      { updating ? 
-        <button 
-          className="updatebtn"
-          onClick={() => updateTask(task.id)}
-        >
+      {updating ? (
+        <button className="updatebtn" onClick={() => updateTask(updTask?.id)}>
           Update
-       </button> :
-       <button 
-          className="submitbtn" 
-          onClick={submitTask}
-        > 
+        </button>
+      ) : (
+        <button className="submitbtn" onClick={submitTask}>
           Save
         </button>
-      }
-     
+      )}
+
       {/** <button className="viewbtn" onClick={fetchTasks}>View tasks</button> */}
 
-      {
-        tasks.map( task => {
-          return(
-            <div className="taskcontainer" key={task.id}>
-                <span>{task.text}</span>
-                <button 
-                  className="deletebtn" 
-                  onClick={ () => deleteTask(task.id)}  
-                >
-                  Delete
-                </button> 
-                <button 
-                  className="updatebtn"
-                  onClick={() => updateTask(task.id)}
-                >
-                  Update
-                </button>
-            </div>
-          )
-        })
-      }
+      {tasks.map((task) => {
+        return (
+          <div className="taskcontainer" key={task.id}>
+            <span>{task.text}</span>
+            <button className="deletebtn" onClick={() => deleteTask(task.id)}>
+              Delete
+            </button>
+            {!updating && (
+              <button
+                className="updatebtn"
+                onClick={() => triggerTaskUpdate(task.id)}
+              >
+                Update
+              </button>
+            )}
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default TasksPage
+export default TasksPage;
